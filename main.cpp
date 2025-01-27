ebf537747a0d6e9eefb99b982dccd08a065ed54d
dzisinho <dzisbogdan26@gmail.com>
2025-01-27 20:16:23 +0200

    completed

--- main.cpp
+++ main.cpp
@@ -1,2 +1,72 @@
 
 
+#include <iostream>
+using namespace std;
+
+class CharStack {
+private:
+    char stack[10];  
+    int top;          
+    int maxSize;      
+
+public:
+    CharStack(int size) {
+        maxSize = size;
+        top = -1;  
+    }
+
+    void push(char c) {
+        if (top < maxSize - 1) {
+            stack[++top] = c;
+        }
+        else {
+            cout << "Stack is full!" << endl;
+        }
+    }
+
+    void pop() {
+        if (top >= 0) {
+            top--;
+        }
+        else {
+            cout << "Stack is empty!" << endl;
+        }
+    }
+
+    char peek() {
+        if (top >= 0) {
+            return stack[top];
+        }
+        else {
+            cout << "Stack is empty!" << endl;
+            return '\0';
+        }
+    }
+
+    int size() {
+        return top + 1;
+    }
+
+    void clear() {
+        top = -1;
+    }
+};
+
+int main() {
+    CharStack stack(5);  
+
+    stack.push('X');
+    stack.push('Y');
+    stack.push('Z');
+
+    cout << "Top character: " << stack.peek() << endl;
+    cout << "Stack size: " << stack.size() << endl;
+
+    stack.pop();
+    cout << "After popping, top character: " << stack.peek() << endl;
+
+    stack.clear();
+    cout << "Size after clearing: " << stack.size() << endl;
+
+    return 0;
+}
